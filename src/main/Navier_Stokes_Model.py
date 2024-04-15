2af0451518186e0cb0ac0c99ffdce1f6806cbd97
oriol.juan <oriol.juan@basetis.com>
2024-04-15 17:48:16 +0200

    [REF] Started modelling scrit, moving to another branch.

--- src/main/Navier_Stokes_Model.py
+++ src/main/Navier_Stokes_Model.py
@@ -1,4 +1,5 @@
 
+import os
 import torch
 from pathlib import Path
 import torch.nn as nn
@@ -6,12 +7,19 @@ import numpy as np
 import scipy.io
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+project_path = os.environ.get('PROJECT_PATH')
+data_path = os.environ.get('PROJECT_DATA_PATH')
+
+data = scipy.io.loadmat(Path(data_path) / Path('cylinder_wake.mat'))
+
+
+print('iep')
+### STEP 1: PreProcessing ###
 
 
 
-model_path = Path.cwd() / Path('app') /  Path('models')
-data_path =  Path.cwd() / Path('app') / Path('data')
-
-data = scipy.io.loadmat(data_path / Path('cylinder_wake.mat'))
-
-class Navier_Stokes()
+#class Navier_Stokes()
